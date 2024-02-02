f40262591573672b70552b0095f5cebada5fbcd8
shashank-chutke <shashank@pensieve.id>
2024-02-02 17:16:26 +0530

    changed API and fixed few bugs

--- src/components/content.js
+++ src/components/content.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import Container from './container'
-import Sidebar from './sidebar'
-import { initiateData } from '../features/games/gamesSlice'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import Container from './container';
+import Sidebar from './sidebar';
+import {initiateData} from '../features/games/gamesSlice';
 
 const Content = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const [loading, setLoading] = useState(false);
-    
+    const [search, setSearch] = useState('');
+
     useEffect(() => {
-        setLoading(true)
-        fetch('https://public.connectnow.org.uk/applicant-test/')
+        setLoading(true);
+        fetch(`https://api.rawg.io/api/games?key=8cf7b7364ad342d2802149d149f81776&search=${search}`)
             .then((response) => response.json())
             .then((actualData) => {
-                dispatch(initiateData(actualData))
-                setLoading(false)
+                dispatch(initiateData(actualData));
+                setLoading(false);
             })
             .catch((err) => {
                 console.log(err.message);
             });
-    }, []);
-
-    
+    }, [search]);
 
     return (
         <div>
-
-            {!loading
-                ?
-                <div className='flex flex-row space-x-10' >
-                    <Sidebar  />
-                    <Container  />
-                </div>
-                :
-                <div className='loading'>
-                    <h2 > PLEASE WAIT LOADING VIDEO GAMES....</h2>
-                </div>
-            }
+            <div className="flex flex-row space-x-10 p-10">
+                <Sidebar setSearch={setSearch} />
+                {!loading ? (
+                    <>
+                        <Container search={search} />
+                    </>
+                ) : (
+                    <div>
+                        <div className="loading">
+                            <h2> PLEASE WAIT LOADING VIDEO GAMES....</h2>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Content
+export default Content;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,37 +1,25 @@
-import React from 'react'
-import "./navbar.css"
-import { NavLink } from "react-router-dom";
-
+import React from 'react';
+import './navbar.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
+    return (
+        <nav className="nav-container p-10 w-100">
+            <NavLink to="/" className={({isActive}) => (isActive ? '' : 'hide')}>
+                <div className="nav-item-1 vis">
+                    <h2>VIDEO GAME</h2>
+                    <span className="nav-title-shadow hide">VIDEO</span>
+                </div>
+            </NavLink>
 
-  return (
-    
-    <nav className="nav-container" >
-      <NavLink
-        to="/"
-        className={({ isActive }) => (isActive ? "" : "hide")}
-      >
+            <NavLink to="/contact" className={({isActive}) => (isActive ? '' : 'hide')}>
+                <div className="nav-item-2 vis visible">
+                    <h2>CONTACT</h2>
+                    <span className="nav-title-shadow hide">CONTACT</span>
+                </div>
+            </NavLink>
+        </nav>
+    );
+};
 
-        <div className="nav-item-1 vis">
-          <h2>VIDEO GAME</h2>
-          <span className="nav-title-shadow hide">VIDEO</span>
-        </div>
-      </NavLink>
-
-
-      <NavLink
-        to="/contact"
-        className={({ isActive }) => (isActive ? "" : "hide")}
-      >
-        <div className="nav-item-2 vis visible">
-          <h2>CONTACT</h2>
-          <span className="nav-title-shadow hide">CONTACT</span>
-        </div>
-      </NavLink>
-      
-    </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
